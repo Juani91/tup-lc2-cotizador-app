d8e2b92521f8b19310264cae99a7ec1bbcc72c45
Facundo Solari <facusolari9@gmail.com>
2024-06-30 16:47:34 -0300

    formulario con api, validacion del mail

--- js/Contacto.js
+++ js/Contacto.js
@@ -1,3 +1,39 @@
-let nombre=document.getElementById("nombre")
-let email=document.getElementById("email")
-let mensaje=document.getElementById("mensaje")
+document.addEventListener('DOMContentLoaded', function() {
+document.getElementById('enviar').addEventListener('click', function(event) {
+    event.preventDefault(); // Evitar que se recargue la página al enviar el formulario
+    
+    // Validar el campo de email
+    email = document.getElementById('email').value.trim();
+    nombre= document.getElementById('nombre').value,
+    mensaje= document.getElementById('mensaje').value
+    function validarMail(email) {
+        emailValidado = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailValidado.test(email);
+      }
+    if (!validarMail(email)) {
+      alert('Por favor ingrese e-mail válido.');
+      return;
+    }
+    // Recoger los valores del formulario
+    formulario = {
+      nombre: nombre,
+      email: email,
+      mensaje: mensaje
+    };
+  
+    // Enviar el formulario usando EmailJS
+    emailjs.send('service_x1dtktq', 'template_dag5nc4', formulario, 'WDMuRSBxfzzsEv0Pb')
+      .then(function(response) {
+        console.log('¡Correo enviado con éxito!', response.status, response.text);
+        alert('¡Correo enviado con éxito!');
+        // Puedes agregar aquí cualquier otra acción que desees después de enviar el correo
+      }, function(error) {
+        console.error('¡Error al enviar el correo!', error);
+        alert('¡Error al enviar el correo! Por favor, inténtalo de nuevo más tarde.');
+      });
+  
+    // Limpiar el formulario después de enviar
+    document.getElementById('formulario').reset();
+  });
+});
+
--- js/common.js
+++ js/common.js
@@ -1,2 +1,2 @@
-
+enviarMail = document.gete
 
